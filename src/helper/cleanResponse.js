9685d4a41a9ffa1fadfa8ccd04285f50d7afc45d
MOSHEUR RAHMAN WOLIED <21301426@bracu.ac.bd>
2023-08-31 13:25:45 +0600

    fixed API cleaning

--- src/helper/cleanResponse.js
+++ src/helper/cleanResponse.js
@@ -3,7 +3,7 @@ cleanSchedule = (response) => {
 
 	// console.log(times);
 
-	let schedule = response.rows.map((row) => {
+	let schedules = response.rows.map((row) => {
 		let schedule = {
 			courseCode: row.cell[1],
 			courseTitle: row.cell[2],
@@ -37,7 +37,19 @@ cleanSchedule = (response) => {
 						day = 'Sat';
 						break;
 				}
-				let [startTime, endTime] = cls.split('-');
+				let clsSplit = cls.split('-')
+				let startTime;
+				let endTime;
+
+				if(clsSplit.length ===2) {
+					startTime = clsSplit[0];
+					endTime = clsSplit[1];
+				}else{
+					let times = cls.split(/(?<=AM|PM)(?=\d)/g)
+					startTime = times[0].split('-')[0]
+					endTime = times[1].split('-')[1]
+				}		
+
 				return {
 					day,
 					startTime,
@@ -48,14 +60,18 @@ cleanSchedule = (response) => {
 		schedule.classes = classes.filter((cls) => cls);
 		return schedule;
 	});
-	// let labs = schedule.filter((schedule) => schedule.classes.length === 1);
-	// labs.forEach((lab) => {
-	// 	const theory = schedule.find((schedule) => {
-	// 		return schedule.courseCode === lab.courseCode && schedule.section === lab.section && schedule.classes.length === 2;
-	// 	});
-	// });
-
-	return schedule;
+	let labs = schedules.filter((schedule) => schedule.classes.length === 1);
+	labs.forEach((lab, index) => {
+		const theory = schedules.find((schedule) => {
+			return schedule.courseCode === lab.courseCode && schedule.section === lab.section && schedule.classes.length === 2;
+		});
+		if (theory){
+			theory.classes.push(lab.classes[0])
+		}
+		schedules.splice(index, 1)
+	});
+// 11:00 AM-12:20 PM12:30 PM-01:50 PM	
+	return schedules;
 };
 
 module.exports = { cleanSchedule };
